49cece5b428105d5ce47436f82a5f75a2beb3819
Kenny Sheftel <kennysheftel@gmail.com>
2024-02-28 17:51:43 -0800

    initial commit for SDL Graphics Renderer

---
+++ src/display.h
@@ -0,0 +1,24 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+extern uint32_t *color_buffer;
+extern SDL_Texture *color_buffer_texture;
+
+extern int window_width;
+extern int window_height;
+
+bool initialize_window(void);
+
+void drawGrid(uint32_t gridColor);
+void drawRect(int x, int y, int width, int height, uint32_t color);
+void render_color_buffer(void);
+void clear_color_buffer(uint32_t color);
+void destroy_window(void);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include "display.h"
+
+bool is_running = NULL;
+
+void setup(void){
+	// Allocate the required bytes in memory for the color buffer
+	color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+	
+	if (!color_buffer) {
+		fprintf(stderr, "Error allocating memory for color_buffer. \n");
+	}
+
+	color_buffer_texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
+}
+
+void process_input(void){
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch(event.type) {
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if(event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = false;
+			break;
+	}
+}
+
+void update(void){
+
+}
+
+void render(void){
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	render_color_buffer();
+	clear_color_buffer(0xFF000000);
+	drawGrid(0xFF404040);
+	drawRect(600,50, 250,150, 0xFFFFFF00);
+	SDL_RenderPresent(renderer);
+}
+
+int main(void) {
+	is_running = initialize_window();
+
+	// game loop
+	setup();
+
+	while(is_running){
+		process_input();
+		update();
+		render();
+	}
+
+	destroy_window();
+	return 0;
+}
