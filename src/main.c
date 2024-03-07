b859897611ab5381b0382129aebcf45507262a56
Kenny Sheftel <kennysheftel@gmail.com>
2024-03-06 21:33:47 -0800

    feat - dynamic mesh arrays

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
+#include "array.h"
+#include "display.h"
+#include "mesh.h"
+#include "vector.h"
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
-#include "display.h"
-#include "mesh.h"
-#include "vector.h"
-
-triangle_t triangles_to_render[N_MESH_FACES];
+//  Array of triangles that should be rendered frame by frame
+triangle_t *triangles_to_render = NULL;
 
 vec3_t camera_position = {.x = 0, .y = 0, .z = -10};
-vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
 
 float fov_factor = 640;
 
@@ -32,6 +32,9 @@ void setup(void) {
     color_buffer_texture =
         SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
                           SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
+
+    // loads the cube values into the mesh data structure
+    load_cube_mesh_data();
 }
 
 void process_input(void) {
@@ -64,18 +67,22 @@ void update(void) {
 
     previous_frame_time = SDL_GetTicks();
 
-    cube_rotation.x += 0.01;
-    cube_rotation.y += 0.01;
-    cube_rotation.z += 0.01;
+    triangles_to_render = NULL;
+
+    mesh.rotation.x += 0.015;
+    mesh.rotation.y += 0.015;
+    mesh.rotation.z += 0.015;
+
+    int num_faces = array_length(mesh.faces);
 
     //  loop all triangles faces
-    for (int i = 0; i < N_MESH_FACES; i++) {
-        face_t mesh_face = mesh_faces[i];
+    for (int i = 0; i < num_faces; i++) {
+        face_t mesh_face = mesh.faces[i];
 
         vec3_t face_vertices[3];
-        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
-        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
-        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
+        face_vertices[0] = mesh.vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh.vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh.vertices[mesh_face.c - 1];
 
         triangle_t projected_triangle;
         // loop all three vertices of this current face and apply
@@ -83,9 +90,9 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec3_t transformed_vertex = face_vertices[j];
 
-            transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
-            transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
-            transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
+            transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
 
             // translate the vertex away from the camera
             transformed_vertex.z -= camera_position.z;
@@ -102,15 +109,16 @@ void update(void) {
         }
 
         //  save the projected triangle in the array of triangles to render.
-        triangles_to_render[i] = projected_triangle;
+        array_push(triangles_to_render, projected_triangle);
     }
 }
 
 void render(void) {
     draw_grid(0xFF404040);
 
+    int num_triangles = array_length(triangles_to_render);
     // loop all projected triangles and render them
-    for (int i = 0; i < N_MESH_FACES; i++) {
+    for (int i = 0; i < num_triangles; i++) {
         triangle_t triangle = triangles_to_render[i];
 
         draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
@@ -126,12 +134,24 @@ void render(void) {
         );
     }
 
+    // clear the array of triangles to render every frame loop
+    array_free(triangles_to_render);
+
     render_color_buffer();
 
     clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
 }
+
+// Free the memory that was dynamically allocated
+void free_resources(void) {
+    free(color_buffer);
+    array_free(mesh.faces);
+    array_free(mesh.vertices);
+}
+
+//
 
 int main(void) {
     is_running = initialize_window();
@@ -146,5 +166,6 @@ int main(void) {
     }
 
     destroy_window();
+    free_resources();
     return 0;
 }
