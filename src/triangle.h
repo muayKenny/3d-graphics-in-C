4cee419416eeee9925e6218a0869e0deea6fe655
Kenny Sheftel <kennysheftel@gmail.com>
2026-01-15 12:18:05 -0800

    zbuffer for draw flat shaded triangle

--- src/triangle.h
+++ src/triangle.h
@@ -7,7 +7,8 @@
 
 #include "vector.h"
 
-typedef struct {
+typedef struct
+{
     int a, b, c;
     color_t color;
     tex2_t a_uv;
@@ -15,15 +16,14 @@ typedef struct {
     tex2_t c_uv;
 } face_t;
 
-typedef struct {
+typedef struct
+{
     vec4_t points[3];
     color_t color;
     tex2_t texcoords[3];
-    float avg_depth;
 } triangle_t;
 
-void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2,
-                          color_t color);
+void draw_filled_triangle(int x0, int y0, float z0, float w0, int x1, int y1, float z1, float w1, int x2, int y2, float z2, float w2, color_t color);
 
 void draw_textured_triangle(int x0, int y0, float z0, float w0, float u0, float v0, int x1, int y1, float z1, float w1,
                             float u1, float v1, int x2, int y2, float z2, float w2, float u2, float v2,
