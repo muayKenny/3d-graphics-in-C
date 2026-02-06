b5f9844d2ffa429c61abe126c7d22720316c1d65
Kenny Sheftel <kennysheftel@gmail.com>
2026-02-05 16:28:44 -0800

    camera implementation

--- src/matrix.c
+++ src/matrix.c
@@ -129,8 +129,10 @@ mat4_t mat4_make_rotation_z(float angle) {
 }
 
 mat4_t mat4_look_at(vec3_t eye, vec3_t target, vec3_t up) {
-    vec3_t z = vec3_normalize(vec3_sub(target, eye));
-    vec3_t x = vec3_normalize(vec3_cross(up, z));
+    vec3_t z = vec3_sub(target, eye);
+    vec3_normalize(&z);
+    vec3_t x = vec3_cross(up, z);
+    vec3_normalize(&x);
     vec3_t y = vec3_cross(z, x);
 
     mat4_t view_matrix = {{{x.x, x.y, x.z, -vec3_dot(x, eye)},
